d9f0055281c49c05acdb7dcad6a7f035efb1de78
Yanfei Guo <yguo@anl.gov>
2025-10-20 21:22:37 -0500

    fix

--- init.sh
+++ init.sh
@@ -4,24 +4,24 @@ mkdir .scripts
 mv * .scripts
 mv .git .scripts
 
-ln -sf ./scripts/env.sh ./env.sh
-ln -sf ./scripts/prepare.sh ./prepare.sh
-ln -sf ./scripts/opt.sh ./opt.sh
-ln -sf ./scripts/bisect_script.sh ./bisect_script.sh
+ln -sf $PWD/.scripts/env.sh ./env.sh
+ln -sf $PWD/.scripts/prepare.sh ./prepare.sh
+ln -sf $PWD/.scripts/opt.sh ./opt.sh
+ln -sf $PWD/.scripts/bisect_script.sh ./bisect_script.sh
 
 mkdir mpich
-ln -sf ./scripts/mpich/build.sh ./mpich/build.sh
-ln -sf ./scripts/mpich/jbuild.sh ./mpich/jbuild.sh
+ln -sf $PWD/.scripts/mpich/build.sh ./mpich/build.sh
+ln -sf $PWD/.scripts/mpich/jbuild.sh ./mpich/jbuild.sh
 
 mkdir ompi
-ln -sf ./scripts/ompi/build.sh ./ompi/build.sh
+ln -sf $PWD/.scripts/ompi/build.sh ./ompi/build.sh
 
 mkdir ucx
-ln -sf ./scripts/ucx/build.sh ./ucx/build.sh
+ln -sf $PWD/.scripts/ucx/build.sh ./ucx/build.sh
 
 mkdir libfabric
-ln -sf ./scripts/libfabric/build.sh ./libfabric/build.sh
+ln -sf $PWD/.scripts/libfabric/build.sh ./libfabric/build.sh
 
 mkdir osu
-ln -sf ./scripts/osu/build.sh ./osu/build.sh
-ln -sf ./scripts/osu/run.sh ./osu/run.sh
+ln -sf $PWD/.scripts/osu/build.sh ./osu/build.sh
+ln -sf $PWD/.scripts/osu/run.sh ./osu/run.sh
